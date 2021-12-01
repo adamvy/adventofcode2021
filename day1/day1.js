ba3bcaca1adcb7ff80115c870bccfa81bba49adb
Adam Van Ymeren <adam@vany.ca>
2021-11-30 22:06:42 -0800

    simpler loop cause i'm never happy

--- day1/day1.js
+++ day1/day1.js
@@ -4,12 +4,10 @@ let input = (await getInput(1)).split("\n").map((n) => parseInt(n));
 
 let count = 0;
 let last;
+
 for (let depth of input) {
-  if (last == undefined) last = depth;
-  else {
-    if (depth > last) count++;
-    last = depth;
-  }
+  if (last != undefined && last < depth) count++;
+  last = depth;
 }
 
 console.log(count);
@@ -18,10 +16,10 @@ count = 0;
 last = undefined;
 
 for (let i = 0; i < input.length - 2; i++) {
-  let s = input[i] + input[i + 1] + input[i + 2];
-  if (last == undefined) last = s;
-  else if (s > last) count++;
-  last = s;
+  let depth = input[i] + input[i + 1] + input[i + 2];
+
+  if (last != undefined && last < depth) count++;
+  last = depth;
 }
 
 console.log(count);
